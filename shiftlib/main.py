c0abfdff4d2d8afabc1e292b4c3c384d9e577d19
hlebnikovdaniil@mail.ru <dkhlebnikov@head02.(none)>
2023-04-18 10:42:54 +0300

    Revise scripts

--- shiftlib/main.py
+++ shiftlib/main.py
@@ -1,18 +1,32 @@
+import sys
 import glob
+import time
 import random
-import subprocess
+import logging
 import shlex as sh
-import concurrent.futures as cf
 import pandas as pd
-from sys import argv
+import subprocess as sp
+import concurrent.futures as cf
 
-from annotTAD import INTERSECT_W_TAD
 from shift_intersect import SHIFT_THEN_INTERSECT
 
-ChIP_dir = argv[1]
-RIP_dir = argv[2]
-RD_dir = argv[3]
-TMP_dir = argv[4]
+
+# READ CMD ARGUMENTS
+ChIP_dir = sys.argv[1]
+RIP_dir = sys.argv[2]
+RD_dir = sys.argv[3]
+TMP_dir = sys.argv[4]
+DPt_dir = sys.argv[5]
+idx = TMP_dir.split('_')[-1]
+
+# SET UP LOGS
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.FileHandler(f"LOGS_SHIFT_{idx}.sh.txt")
+handler.setLevel(logging.DEBUG)
+root.addHandler(handler)
+logging.info(f"ARGUMENTS passsed: {ChIP_dir, RIP_dir, RD_dir, TMP_dir, DPt_dir}...")
 
 # COMMON DEFS
 aux_files_dir = "/home/dkhlebnikov/TRIADS/SHIFT_AUX_FILES"
@@ -22,31 +36,26 @@ K562_path = f"{aux_files_dir}/K562.compartments.bed"
 mESC_path = f"{aux_files_dir}/mESC.compartments.bed"
 
 # MAKE WORKING DIRS
-subprocess.run(sh.split(f"mkdir {TMP_dir}"))
-subprocess.run(sh.split(f"mkdir {TMP_dir}/DP_tads"))
-subprocess.run(sh.split(f"mkdir {TMP_dir}/RP_peaks"))
-subprocess.run(sh.split(f"mkdir {TMP_dir}/DP_peaks"))
+sp.run(sh.split(f"mkdir {TMP_dir}"))
+sp.run(sh.split(f"mkdir {TMP_dir}/RP_peaks"))
+sp.run(sh.split(f"mkdir {TMP_dir}/DP_peaks"))
+logging.info("DIRS created, PREREQUISITES defined...")
 
 # GET LIST OF PROTEINS AND ORGANISMS
 PROT_ORG = []
 for el in glob.glob(f"{ChIP_dir}/*"):
-  fname = el.split('.')[2]
+  fname = el.split('/')[1]
   prot =  fname.split('.')[0]
   org =   fname.split('.')[2]
   PROT_ORG.append((prot, org))
 
-  # RUN ChIP-SEQ INTERSECT WITH TADs
-  if org == "K562":
-    INTERSECT_W_TAD(el, K562_path, f"{TMP_dir}/DP_tads/{fname}")
-  elif org == "mESC":
-    INTERSECT_W_TAD(el, mESC_path, f"{TMP_dir}/DP_tads/{fname}")
-
 # DRIVER CODE
 sim_res = []
-for i in tqdm(range(10_000)):
-  
+for i in range(20):
+  logging.info(f"SHIFTING started {i+1}-th time. Shifting & Intersecting...")
+  init_time = time.time()
+
   # GENERATE SHIFT DICT FOR BOTH ORGANISMS
-  random.seed(SEED)
   shifts = {}
   shifts["K562"] = { IP : {ch : 0 for ch in chr_org["K562"]} for IP in ("ChIP", "RIP")}
   shifts["mESC"] = { IP : {ch : 0 for ch in chr_org["mESC"]} for IP in ("ChIP", "RIP")}
@@ -63,21 +72,33 @@ for i in tqdm(range(10_000)):
   # GENERATE ARGUMENTS FOR SHIFTS AND INTERSECTS
   exec_arguments = []
   for el in PROT_ORG:
-    prot, org = PROT_ORG[0], PROT_ORG[1]
+    prot, org = el[0], el[1]
     init_locs = {"RIP" : glob.glob(f"{RIP_dir}/{prot}*{org}*bed")[0],
-                 "ChIP" : glob.glob(f"{ChIP_dir}/{prot}.ChIP.{org}.bed")[0]}
+                 "ChIP" : glob.glob(f"{DPt_dir}/{prot}.ChIP.{org}.bed")[0]}
     out_locs = {"RIP" : f"{TMP_dir}/RP_peaks",
                 "ChIP" : f"{TMP_dir}/DP_peaks"}
     rd_path = f"{RD_dir}/grid_mm10_mESC.bed" if org == "mESC" else f"{RD_dir}/redc_hg38_K562.bed"
     exec_arguments.append((el, init_locs, out_locs, shifts[org], chr_org[org], rd_path, aux_files_dir))
 
   # SHIFT PEAKS AND INTERSECT
-  with cf.ThreadPoolExecutor() as executor:
-    futures = {executor.submit(SHIFT_INTERSECT, *argts) for argts in exec_arguments}
-    
-    for futr in cf.as_completed(futures):
-      triad_cnt = futr.result()
-      sim_res.append((i, triad_cnt[0], triad_cnt[1]))
+  with cf.ProcessPoolExecutor(max_workers=72) as executor:
+    futures = {executor.submit(SHIFT_THEN_INTERSECT, *argts) for argts in exec_arguments}
+  
+  time_taken = time.time() - init_time
+  logging.info(f"Shifts and intersects done (Time: {time_taken}). Writing!")    
+  for futr in cf.as_completed(futures):
+    triad_cnt = futr.result()
+    sim_res.append((i, triad_cnt[0], triad_cnt[1]))
 
-pd.DataFrame(sim_res).to_csv("sim_res.tsv", sep='\t', header=False, index=False)
+  removal_rip =  f"rm {TMP_dir}/*P_peaks/* -rf"
+  remove_sp = sp.Popen(sh.split(removal_rip))
+  _, _ = remove_sp.communicate()
+
+# CLEAN UP
+clean_up =  f"rm {TMP_dir} -rf"
+clean_up_proc = sp.Popen(sh.split(clean_up))
+_, _ = clean_up_proc.communicate()
+
+# SAVE RESULTS
+pd.DataFrame(sim_res).to_csv(f"sim_res_{idx}.tsv", sep='\t', header=False, index=False)
 
--- shiftlib/shift_intersect.py
+++ shiftlib/shift_intersect.py
@@ -1,7 +1,9 @@
 import os
+import sys
 import math
 import glob
 import random
+import logging
 import pandas as pd
 import shlex as sh
 import subprocess as sp
@@ -25,23 +27,27 @@ def SHIFT_THEN_INTERSECT(PROT_ORG, INIT_LOCS, OUTDIR, SHIFTS_DICT, CHR_LIST, RD_
   fname = INIT_LOCS["RIP"].split('/')[-1]
 
   # SHIFT RIP PEAKS
-  SHIFT_RNA_Protein(INIT_LOCS["RIP"], f"{OUTDIR['RIP']}/fname", SHIFTS_DICT["RIP"], CHR_LIST)
+  SHIFT_RNA_Protein(INIT_LOCS["RIP"], f"{OUTDIR['RIP']}/{fname}", SHIFTS_DICT["RIP"], CHR_LIST)
   SHIFT_DNA_Protein(INIT_LOCS["ChIP"], f"{OUTDIR['ChIP']}/{PROT}.ChIP.{ORG}.bed", SHIFTS_DICT["ChIP"], chip_TADmap)
 
   # INTERSECT SHIFTED
   intsec_DP_cmd = f"{intersectBed} -a {RD_DATA} -b {OUTDIR['ChIP']}/{PROT}.ChIP.{ORG}.bed -wb"
-  rearr_cmd = "awk '{print $4"\t"$5"\t"$6"\t"$7"\t"$1"\t"$2"\t"$3"\t"$8"\t"$9"\t"$10"\t"$11"\t"$19"\t"$20}'"
-  intsec_RP_cmd = f"{intersectBed} -a stdin -b {OUTDIR['RIP']}/fname -wb"
-  rear_filt_cmd = "awk '$4 == $17 {print $1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"$10"\t"$11"\t"$12"\t"$13"\t"$18"\t"$19}'"
+  rearr_cmd = "awk \'$9 != \"protein_coding\" {print $4\"\t\"$5\"\t\"$6\"\t\"$7\"\t\"$1\"\t\"$2\"\t\"$3\"\t\"$8\"\t\"$9\"\t\"$10\"\t\"$11\"\t\"$19\"\t\"$20}\'"
+  intsec_RP_cmd = f"{intersectBed} -a stdin -b {OUTDIR['RIP']}/{fname} -wb"
+  awk_filt_cmd = "awk \'$4 == $17\'"
   wc_cmd = "wc -l"
 
-  intersect_ChIP = sp.Popen(sh.split(intsec_DP_cmd),                                stdout = sp.PIPE)
-  rearr_1        = sp.Popen(sh.split(rearr_cmd),     stdin = intersect_ChIP.stdout, stdout = sp.PIPE)
-  intersect_RIP  = sp.Popen(sh.split(intsec_RP_cmd), stdin = rearr_1.stdout,        stdout = sp.PIPE)
-  rear_filt      = sp.Popen(sh.split(rear_filt_cmd), stdin = intersect_RIP.stdout,  stdout = sp.PIPE)
-  wc_results     = sp.Popen(sh.split(wc_cmd),        stdin = rear_filt.stdout,      stdout = sp.PIPE, text = True)
+  intersect_ChIP = sp.Popen(sh.split(intsec_DP_cmd),                                stdout = sp.PIPE, text = True)
+  rearrange      = sp.Popen(sh.split(rearr_cmd),     stdin = intersect_ChIP.stdout, stdout = sp.PIPE, text = True)
+  intersect_ChIP.stdout.close()
+  intersect_RIP  = sp.Popen(sh.split(intsec_RP_cmd), stdin = rearrange.stdout,      stdout = sp.PIPE, text = True)
+  rearrange.stdout.close()
+  awk_filt       = sp.Popen(sh.split(awk_filt_cmd),  stdin = intersect_RIP.stdout,  stdout = sp.PIPE, text = True)
+  intersect_RIP.stdout.close()  
+  wc_results     = sp.Popen(sh.split(wc_cmd),        stdin = awk_filt.stdout,  stdout = sp.PIPE, text = True)
+  awk_filt.stdout.close()
 
   triad_count, _ = wc_results.communicate()
-  return (f"{PROT}_{ORG}", triad_count)
+  return (f"{PROT}_{ORG}", triad_count.strip())
 
   
